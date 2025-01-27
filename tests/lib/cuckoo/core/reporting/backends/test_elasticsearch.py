c96068f051bd2040432d2a6fb6138744fe2e501b
Nick Bargnesi <nbargnesi@secureworks.com>
2025-01-27 16:29:33 -0500

    add basic tests, use getattribute vice getattr

---
+++ tests/lib/cuckoo/core/reporting/backends/test_elasticsearch.py
@@ -0,0 +1,18 @@
+import functools
+import inspect
+
+from lib.cuckoo.core.reporting import api
+from lib.cuckoo.core.reporting.backends import elasticsearch
+
+getfunctions = functools.partial(inspect.getmembers, predicate=inspect.isfunction)
+
+
+class TestMongoDBReportingBackend:
+    def test_has_api_methods(self):
+        api_functions = {x for x in getfunctions(api.Reports)}
+        assert len(api_functions) > 0
+        backend = elasticsearch.ElasticsearchReports
+        for fn in api_functions:
+            assert hasattr(backend, fn[0])
+            msg = f"reporting backend {backend} does not implement '{fn[0]}'"
+            assert getattr(backend, fn[0]) != fn[1], msg
