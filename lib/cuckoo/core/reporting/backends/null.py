f9dcf2fa141b71a2413d08d48449abc30f6836ee
Nick Bargnesi <nbargnesi@secureworks.com>
2025-01-27 16:29:33 -0500

    add basic tests, use getattribute vice getattr

--- lib/cuckoo/core/reporting/backends/null.py
+++ lib/cuckoo/core/reporting/backends/null.py
@@ -3,9 +3,12 @@ from lib.cuckoo.common.exceptions import CuckooOperationalError
 from lib.cuckoo.core.reporting import api
 
 
-class Null(api.Reports):
-    def __init__(self, cfg: config.Config):
+class NullReports(api.Reports):
+    def __init__(self, _: config.Config):
         pass
 
-    def __getattr__(self, name):
-        raise CuckooOperationalError("reporting backend does not support '%s'" % name)
+    def __getattribute__(self, name):
+        def fn(*args, **kwargs):
+            raise CuckooOperationalError("reporting backend does not support '%s'" % name)
+
+        return fn
