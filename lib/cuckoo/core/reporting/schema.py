4664b6c60619a64b1367c5610871e5363b84a74f
Robin Koumis <rkoumis@secureworks.com>
2025-01-31 16:27:54 -0500

    Added info schema nested in summary

--- lib/cuckoo/core/reporting/schema.py
+++ lib/cuckoo/core/reporting/schema.py
@@ -1,26 +1,57 @@
+import abc
+import datetime
 from typing import List, Optional
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel as PydanticBaseModel
+from pydantic import Extra
+
+
+class BaseModel(PydanticBaseModel, abc.ABC):
+    class Config:
+        allow_population_by_field_name = True
+        extra = Extra.allow
+
+
+class Machine(BaseModel):
+    id: Optional[int]
+    status: Optional[str]
+    name: Optional[str]
+    label: Optional[str]
+    platform: Optional[str]
+    manager: Optional[str]
+    started_on: Optional[datetime.datetime]
+    shutdown_on: Optional[datetime.datetime]
+
+
+class Info(BaseModel):
+    id: Optional[int]
+    version: Optional[str]
+    started: Optional[datetime.datetime]
+    ended: Optional[datetime.datetime]
+    duration: Optional[int]
+    category: Optional[str]
+    machine: Optional[Machine]
+    package: Optional[str]
+    timeout: Optional[bool]
+    tlp: Optional[str]
+    user_id: Optional[int]
 
 
 class Summary(BaseModel):
-    vt_file_summary: Optional[str] = Field(validation_alias="target.file.virustotal.summary")
-    vt_url_summary: Optional[str] = Field(validation_alias="url.virustotal.summary")
+    info: Optional[Info]
+    vt_file_summary: Optional[str]
+    vt_url_summary: Optional[str]
     malscore: Optional[float]
-    detections: Optional[List[str]]
-    pcap_sha256: Optional[str] = Field(validation_alias="network.pcap_sha256")
+    detections: Optional[List[dict]]
+    pcap_sha256: Optional[str]
     mlist_cnt: Optional[int]
     f_mlist_cnt: Optional[int]
-    clamav: Optional[List[str]] = Field(validation_alias="target.file.clamav")
+    clamav: Optional[List[str]]
     suri_tls_cnt: Optional[int]
     suri_alert_cnt: Optional[int]
     suri_http_cnt: Optional[int]
     suri_file_cnt: Optional[int]
     trid: Optional[int]
-
-
-class Info(BaseModel):
-    ...
 
 
 class AnalysisConfigs(BaseModel):
