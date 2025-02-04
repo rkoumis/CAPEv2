d854583826c5120e91c690218b84dc78cf3ef97a
Josh Feather <142008135+josh-feather@users.noreply.github.com>
2025-02-04 13:47:52 +0000

    Move BaseModel to Pydantic 2 syntax

--- lib/cuckoo/core/reporting/schema.py
+++ lib/cuckoo/core/reporting/schema.py
@@ -2,14 +2,12 @@ import abc
 import datetime
 from typing import List, Optional
 
-from pydantic import BaseModel as PydanticBaseModel
+from pydantic import BaseModel as PydanticBaseModel, ConfigDict
 from pydantic import Extra
 
 
 class BaseModel(PydanticBaseModel, abc.ABC):
-    class Config:
-        allow_population_by_field_name = True
-        extra = Extra.allow
+    model_config = ConfigDict(extra='allow', populate_by_name=True)
 
 
 class Machine(BaseModel):
