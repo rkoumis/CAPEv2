a3b39a23f31b0f8724ff44d52ff3671b09f6bbc6
Josh Feather <142008135+josh-feather@users.noreply.github.com>
2025-02-10 16:44:57 +0000

    Add new API methods and update existing return types

--- lib/cuckoo/core/reporting/api.py
+++ lib/cuckoo/core/reporting/api.py
@@ -2,6 +2,8 @@ from typing import Generator
 
 from lib.cuckoo.core.reporting import schema
 
+from .types import SearchCategories
+
 
 class Reports:
     """Interface to report backends."""
@@ -15,39 +17,37 @@ class Reports:
     def delete(self, task_id: int) -> bool:
         raise NotImplementedError()
 
-    def search(self, term, value, limit: int = 0, projection=None) -> list:
+    def search(self, term, value, limit: int = 0, projection=None) -> list[schema.Info]:
         raise NotImplementedError()
 
-    def search_by_user(self, term, value, user_id=False, privs=False) -> list:
+    def search_by_category(self, category: SearchCategories, limit: int = 0) -> list[schema.Info]:
+        raise NotImplementedError()
+
+    def search_by_user(self, term, value, user_id=False, privs=False) -> list[schema.Info]:
         raise NotImplementedError()
 
     def search_by_sha256(self, sha256: str, limit: int = 0) -> list[schema.Info]:
         raise NotImplementedError()
 
-    # TODO(jf) implement search by category
-    # (category could be an enum of: file, url, pcap, static
-    def search_by_category(self, category: str, limit: int = 0) -> list[schema.Info]:
+    def search_payloads_by_sha256(self, sha256: str, limit: int = 0) -> list[schema.Info]:
         raise NotImplementedError()
 
-    def search_payloads_by_sha256(self, sha256: str, limit: int = 0) -> list:
+    def search_dropped_by_sha256(self, sha256: str, limit: int = 0) -> list[schema.Info]:
         raise NotImplementedError()
 
-    def search_dropped_by_sha256(self, sha256: str, limit: int = 0) -> list:
+    def search_procdump_by_sha256(self, sha256: str, limit: int = 0) -> list[schema.Info]:
         raise NotImplementedError()
 
-    def search_procdump_by_sha256(self, sha256: str, limit: int = 0) -> list:
+    def search_suricata_by_sha256(self, sha256: str, limit: int = 0) -> list[schema.Info]:
         raise NotImplementedError()
 
-    def search_suricata_by_sha256(self, sha256: str, limit: int = 0) -> list[schema.Suricata]:
+    def search_detections_by_sha256(self, sha256: str, limit: int = 0) -> list[schema.Info]:
         raise NotImplementedError()
 
-    def cape_configs(self, task_id: int) -> list[schema.AnalysisConfig]:
+    def cape_configs(self, task_id: int) -> list[schema.AnalysisConfig] | None:
         raise NotImplementedError()
 
-    def detections_by_sha256(self, sha256: str) -> dict:
-        raise NotImplementedError()
-
-    def iocs(self, task_id: int) -> dict:
+    def iocs(self, task_id: int): # -> schema.IOC | None:
         raise NotImplementedError()
 
     def summary(self, task_id: int) -> schema.Summary | None:
@@ -56,7 +56,10 @@ class Reports:
     def summaries(self) -> Generator[schema.Summary, None, None]:
         raise NotImplementedError()
 
-    def recent_suricata_alerts(self, minutes=60) -> list:
+    def recent_suricata_alerts(self, minutes=60) -> list[schema.Suricata]:
+        raise NotImplementedError()
+
+    def detections(self, task_id: int) -> dict:
         raise NotImplementedError()
 
     def dropped(self, task_id: int) -> dict:
@@ -65,14 +68,16 @@ class Reports:
     def memory(self, task_id: int) -> dict:
         raise NotImplementedError()
 
-    # TODO(jf) return schema.Network in implementations
-    def network(self, task_id: int) -> schema.Network:
+    def network(self, task_id: int) -> schema.Network | None:
+        raise NotImplementedError()
+
+    def payloads(self, task_id: int) -> dict:
         raise NotImplementedError()
 
     def procdump(self, task_id: int) -> dict:
         raise NotImplementedError()
 
-    def procmemory(self, task_id: int) -> dict:
+    def procmemory(self, task_id: int) -> schema.ProcMemory | None:
         raise NotImplementedError()
 
     def calls(self, task_id: int) -> list[schema.Call]:
